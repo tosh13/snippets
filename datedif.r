ad0943dbd7ed5ddd1a7a6a4250e1e78e99ada042
Toshiki Saito <github@jdbase.com>
2015-10-19 06:35:48 +0900

    Update datedif.r

--- datedif.r
+++ datedif.r
@@ -1,3 +1,23 @@
+require(lubridate)
+#
+#= DATEDIF function in R
+#
+#Authors::   Toshiki I. Saito
+#Version::   1.0 2015-10-19 tosh
+#License::   The MIT License
+#--
+# starting: Starting date
+# ending: Ending date
+# y: Years; Complete calendar years between the dates.
+# m: Months; Complete calendar months between the dates.
+# d: Days; Number of days between the dates.
+# ym: Months Excluding Years; Complete calendar months
+#     between the dates as if they were of the same year.
+# yd: Days Excluding Years; Complete calendar days
+#     between the dates as if they were of the same year.
+# md: Days Excluding Years And Months; Complete calendar days
+#     between the dates as if they were of the same month and same year.
+#++
 datedif <- function(starting, ending) {
   y <- as.integer((as.integer(format(ymd(ending),"%Y%m%d")) - as.integer(format(ymd(starting),"%Y%m%d")))/10000)
   months <- as.numeric((as.integer(format(ymd(ending),"%m%d")) - as.integer(format(ymd(starting),"%m%d")))/100)
